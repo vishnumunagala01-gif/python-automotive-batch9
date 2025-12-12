3e2e316f1239d1dbbca684a357850bba8ec78786
vishnumunagala01-gif <vishnumunagala01@gmail.com>
2025-12-12 15:07:14 +0530

    first commit

---
+++ day 2/2d list.py
@@ -0,0 +1,11 @@
+games=[3,4,2]
+offline=[1,6,8]
+online=[12,54,23]
+
+# making lists into a single 2d list
+choice=[games,offline,online]
+
+# printing the 2d list elements
+print(choice)
+print(choice[2][1])
+print(choice[0][0])
---
+++ day 2/arthimetic.py
@@ -0,0 +1,26 @@
+#addition of numbers
+print("Addition of two numbers")
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+total = num1 + num2
+print("Sum of 2 numbers:",total)
+
+#concatination
+print("concatination of two numbers")
+sen1=input("enter the first sentence:")
+sen2=input("enter the secomd sentence:")
+print(sen1+sen2)
+
+#subtraction
+print("subtraction of two numbers")
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+total = num1 - num2
+print("difference of 2 numbers:",total)
+
+#multiplication
+print("multiplication of two numbers")
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+print("multiply of 2 numbers:",num1*num2)
+
